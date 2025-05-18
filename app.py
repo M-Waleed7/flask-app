450461512afa33e662e934312103eab5d1fcf776
Muhammad Waleed <97525639+M-Waleed7@users.noreply.github.com>
2025-05-19 00:08:34 +0500

    Update app.py

--- app.py
+++ app.py
@@ -66,4 +66,4 @@ if __name__ == "__main__":
     init_db()
 
     # Run Flask app
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 9090)))
